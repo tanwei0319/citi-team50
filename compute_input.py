680a9bc6deeb58903fa36988cfdfc140124ec81c
wenqinglim <lim_wenq@hotmail.com>
2017-09-24 00:08:06 -0700

    Test data manipulation on python

--- compute_input.py
+++ compute_input.py
@@ -1,25 +1,30 @@
 ## compute_input.py
 
-import sys, json, numpy as np
+import sys, json, numpy as np, pandas as pd
 
 #Read data from stdin
 def read_in():
     lines = sys.stdin.readlines()
+
     #Since our input would only be having one line, parse our JSON data from that
-    return json.loads(lines(0))
+    return json.loads(lines[0])
 
 def main():
-    #get our data as an array from read_in()
+    #get our data as dictionary from read_in()
     lines = read_in()
 
-    #create a numpy array
-    np_lines = np.array(lines)
+    #initialize empty panda DF
+    df = pd.DataFrame()
 
-    #use numpys sum method to find sum of all elements in the array
-    lines_sum = np.sum(np_lines)
+    for line in lines['transactions']:
+        #convert to panda data frame
+        dfline = pd.DataFrame(line, index=[0])
+        df = df.append(dfline)
 
-    #return the sum to the output stream
-    print (lines_sum)
+    #get selected attributes (transactionAmount, transactionDate, transactionDescription)
+    dffilter = df[['transactionAmount', 'transactionDate', 'transactionDescription']]
+    #return dictionary
+    print (dffilter)
 
 #start process
 if __name__ == '__main__':
