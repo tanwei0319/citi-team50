8464a0c55e7ddc2777f4989d280eb5d847a35d26
wenqinglim <lim_wenq@hotmail.com>
2017-09-23 21:51:12 -0700

    Json sample data

--- compute_input.py
+++ compute_input.py
@@ -6,7 +6,7 @@ import sys, json, numpy as np
 def read_in():
     lines = sys.stdin.readlines()
     #Since our input would only be having one line, parse our JSON data from that
-    return json.loads(lines[0])
+    return json.loads(lines(0))
 
 def main():
     #get our data as an array from read_in()
