d2c311ab282723bdbeb38aabdd90f1aa1f45f766
jindongy <jindongy@gmail.com>
2017-09-24 02:02:40 -0700

    server changes

--- website.js
+++ website.js
@@ -1,26 +1,35 @@
-var express = require("express");
+
+const express = require('express');
+const path = require('path');
+const https = require('https');
+const fs = require('fs');
+const async = require('async');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const querystring = require('querystring');
+const config = require('./config.json');
+
 var app = express();
+
+app.use(express.static(path.join(__dirname, 'views')));
+
+
 var router = express.Router();
   
-var path = __dirname + "/views/";
   
 app.use("/",router);
   
 router.get("/",function(req, res){
-  res.sendFile(path + "index.html");
+  res.sendFile(path.join(__dirname + "/views/main.html"));
 });
   
 router.get("/product",function(req, res){
-  res.sendFile(path + "product.html");});
-  
-router.get("/about",function(req, res){
-  res.sendFile(path + "about.html");
-});
+  res.sendFile(path.join(__dirname + "/views/product.html"));});
   
 app.use("*",function(req, res){
   res.send("Error 404: Not Found!");
 });
   
-app.listen(3000,function(){
-  console.log("Server running at Port 3000");
-});
+// app.listen(3000,function(){
+//   console.log("Server running at Port 3000");
+// });
